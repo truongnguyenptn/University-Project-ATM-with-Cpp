a1af619a77fb1f450837ded241a4508ed36d9f4e
truongnguyenp <truongnguyen310.work@gmail.com>
2022-03-12 09:53:17 +0700

    Add module structure

---
+++ Src/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "ModuleUser.h"
+using namespace std;
+
+int main(){
+  User *user = new User(102210082,100,0);
+ int n;
+ cin>>n;
+ cout<<n;
+ return 0;
+}
+
---
+++ Src/ModuleUser.h
@@ -0,0 +1,29 @@
+#ifndef MODULEUSER_H
+#define MODULEUSER_H
+class User {
+	private: 
+	int ACNum;
+	int PINs;
+	long long balance;
+	public:
+	User() {
+	}
+	User(int ACNum, int PINs, long long balance) {
+		
+	}
+	void getBalance() {
+		
+	}
+	void withdraw(int money) {
+	
+		
+	}
+	void deposit(int money) {
+		
+	}
+	void display() {
+	
+	}
+	
+};
+#endif
